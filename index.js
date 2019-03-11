debcb25607b95371122c873d4799e54f979fb510
Yeshi <tyeshi777@yahoo.com>
2019-03-11 13:48:23 -0700

    added some images and paras

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 let displayDate = new Date();
 document.getElementById("date").innerHTML = displayDate.toLocaleString();
 
-function openSlideMenu() {
-  document.getElementById("side-menu").style.width = "150px";
-  document.getElementById("main").style.marginLeft = "150px";
-}
+// function openSlideMenu() {
+//   // document.getElementById("side-menu").style.marginBottom = "150px";
+//   document.getElementById("main").style.marginTop = "150px";
+// }
 
-function closeSlideMenu() {
-  document.getElementById("side-menu").style.width = "0";
-  document.getElementById("").style.marginLeft = "0";
-}
+let hiddenDiv = document.getElementById("hiddenDiv");
+hiddenDiv.addEventListener("click", function(event) {
+  event.target.style.marginBottom = "150px";
+  event.target.style.textContent = "Hello";
+});
+
+// function closeSlideMenu() {
+//   document.getElementById("side-menu").style.marginBottom = "0";
+//   document.getElementById("").style.marginTop = "0";
+// }
+
+let signUp = document.getElementById("signUp");
+signUp.addEventListener("click", function(event) {
+  event.preventDefault();
+  document.getElementById("signUpHidden").style.display = "block";
+  document.querySelector(".signinbtn").style.display = "none";
+});
+
+let signIn = document.getElementById("signIn");
+signIn.addEventListener("click", function(event) {
+  event.preventDefault();
+  document.getElementById("signUpHidden").style.display = "block";
+  document.querySelector(".signupbtn").style.display = "none";
+});
