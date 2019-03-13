2e167d41a68b43862c60e597bf3fbd9b5e3f8de9
Yeshi <tyeshi777@yahoo.com>
2019-03-13 15:03:16 -0700

    still looking at checkbox

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ class AboutImg {
     this.itemData = this.item.dataset.tab;
 
     this.content = document.querySelectorAll(
-      // `.content[data-tab='${this.item.dataset.tab}']`
       `.content[data-tab='${this.itemData}']`
     );
     this.content = Array.from(this.content).map(item => new Content(item));
-    // this.content = new Content(this.content);
     this.item.addEventListener("click", () => this.itemClick());
   }
   itemClick() {
-    // this.content.toggleContent();
     const content = document.querySelectorAll(".content");
     content.forEach(item => (item.style.display = "none"));
     this.content.forEach(item => item.displayContent());
@@ -23,7 +20,6 @@ class Content {
     this.content = content;
   }
   displayContent() {
-    // this.content.classList.toggle("change");
     this.content = this.content.style.display = "block";
   }
 }
@@ -31,8 +27,17 @@ const aboutImg = document
   .querySelectorAll(".aboutImg")
   .forEach(item => new AboutImg(item));
 
-let displayDate = new Date();
-document.getElementById("date").innerHTML = displayDate.toString();
+// document.getElementById("date").innerHTML = Date();
+function display_c() {
+  var refresh = 1000; // Refresh rate in milli seconds
+  mytime = setTimeout("display_ct()", refresh);
+}
+
+function display_ct() {
+  var x = new Date();
+  document.getElementById("ct").innerHTML = x;
+  display_c();
+}
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -41,3 +46,11 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+const contact = document.getElementById("contact");
+contact.addEventListener("mouseover", function(event) {
+  event.target.style.textTransform = "uppercase";
+});
+contact.addEventListener("mouseout", function(event) {
+  event.target.style.textTransform = "lowercase";
+});
